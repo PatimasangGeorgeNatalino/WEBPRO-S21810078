2c11e8c2205a9bf334d0f2af7491ebdc7be45486
PatimasangGeorgeNatalino <s21810078@student.unklab.ac.id>
2020-09-10 18:18:51 +0800

    added readme file

---
+++ pertemuan3/index.js
@@ -0,0 +1,12 @@
+const firstName = "george";
+const lastName = "patimasang";
+let age = 21;
+let status = single;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(status);
+
+const info = " Nama saya adalah " + firstName + lastName + "Umur saya " + age + " tahun, status " + status ;
+console.log(info);
---
+++ pertemuan4/index.js
@@ -0,0 +1,6 @@
+let a = 8;
+let b = 3;
+let LuasA = a + a;
+let LuasB = b + b;
+let total = LuasA + LuasB;
+console.log(total);
