0981b8ebc03a09330eee1cf0d73acc957f05231f
PatimasangGeorgeNatalino <s21810078@student.unklab.ac.id>
2020-11-10 22:39:23 +0800

    committed

---
+++ pertemuan20/src/utils/person.js
@@ -0,0 +1,26 @@
+export const person = [
+    {
+        name = "George",
+        institution = "Unklab",
+        address = "Balikpapan",
+        phoneNumber = "0822xxxxxxxx",
+    },
+    {
+        name = "Yenitha",
+        institution = "Unair",
+        address = "Surabaya",
+        phoneNumber = "0812xxxxxxxx",
+    },
+    {
+        name = "Theresa",
+        institution = "Unsrat",
+        address = "Manado",
+        phoneNumber = "0821xxxxxxxx",
+    },
+    {
+        name = "Bryan",
+        institution = "UI",
+        address = "Jakarta",
+        phoneNumber = "0811xxxxxxxx",
+    }
+];
